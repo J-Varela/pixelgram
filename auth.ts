b02b987b24198c8171f3c7f1c0bd19bd6a59ad65
Jonathan Varela <jonvarela4@gmail.com>
2023-12-26 12:57:56 -0500

    first commit

---
+++ auth.ts
@@ -0,0 +1,66 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from '@/lib/prisma';
+import GoogleProvider from 'next-auth/providers/google';
+import { NextAuthOptions } from 'next-auth';
+import NextAuth from 'next-auth';
+
+export const config = {
+  pages: {
+    signIn: '/login',
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+        session.user.username = token.username;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      const prismaUser = await prisma.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+
+      if (!prismaUser) {
+        token.id = user.id;
+        return token;
+      }
+      if (!prismaUser.username) {
+        await prisma.user.update({
+          where: {
+            id: prismaUser.id,
+          },
+          data: {
+            username: prismaUser.name?.split(' ').join('').toLowerCase(),
+          },
+        });
+      }
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        email: prismaUser.email,
+        username: prismaUser.username,
+        picture: prismaUser.image,
+      };
+    },
+  },
+} as NextAuthOptions;
+
+export default NextAuth(config);
